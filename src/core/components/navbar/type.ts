d29c7b017324d1fab4650bf3d175e177dd30710e
Kanyapat W. <68800250+winnerkypt@users.noreply.github.com>
2025-05-23 23:04:19 +0700

    Revert "init another file"
    This reverts commit 974328f1c5bad2d7ea9bdddeba7f0532db1ddefb.

--- src/core/components/navbar/type.ts
+++
@@ -1,4 +0,0 @@
-export type NavbarType = {
-  label: string;
-  href: string;
-};
--- src/presentation/home/home-page.tsx
+++
@@ -1,8 +0,0 @@
-import { Welcome } from "./components/welcome";
-export const HomePage = () => {
-  return (
-    <div className="flex flex-col overflow-hidden">
-      <Welcome />
-    </div>
-  );
-};
