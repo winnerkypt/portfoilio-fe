fadf1ffbd5b218775c4b3f06c0333e19e5966520
Kanyapat W. <68800250+winnerkypt@users.noreply.github.com>
2025-06-09 13:07:37 +0700

    add font

--- src/core/components/about.tsx
+++ src/core/components/about.tsx
@@ -1,9 +1,11 @@
 "use client"
 
+import { ranchers } from "../libs/fonts";
+
 export const About = () => {
   return (
     <div className="bg-light-yellow ">
-      <h1 className="text-[256px] font-bold text-center text-brown py-10">
+      <h1 className={`${ranchers.className} text-[256px] font-bold text-center text-brown py-10`}>
         KANYAPAT
       </h1>
     </div>
---
+++ src/core/libs/fonts.tsx
@@ -0,0 +1,12 @@
+// lib/fonts.ts
+import { Ranchers, Andika } from "next/font/google";
+export const andika = Andika({
+  subsets: ['latin'],
+  weight: ['400'],
+  display: 'swap',
+})
+export const ranchers = Ranchers({
+  subsets: ['latin'],
+  weight: '400',
+  display: 'swap',
+})
