048d9ee72d43c95633ce90e6c53c8ca131d2b20f
Kanyapat W. <68800250+winnerkypt@users.noreply.github.com>
2025-07-02 20:59:44 +0700

    fix home ui

--- src/core/presentations/main.tsx
+++ src/core/presentations/main.tsx
@@ -1,14 +1,24 @@
 "use client";
 
+import Image from "next/image";
 import { ContactMain } from "../components/main/contact";
 import { NameMain } from "../components/main/name";
 export const Main = () => {
   return (
-    <div className="bg-light-yellow py-9">
-      <div className="container">
-        <ContactMain />
-        <NameMain />
+    <div className="mb-9">
+      <div className="bg-light-yellow pt-12">
+        <div className="container">
+          <ContactMain />
+          <NameMain />
+        </div>
       </div>
+      <Image
+        src="/images/wave.svg"
+        alt="Hero Image"
+        width={100}
+        height={100}
+        className="w-full "
+      />
     </div>
   );
 };
